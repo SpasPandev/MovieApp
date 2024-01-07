33be10d0fa19786833ba29d080fc051bba0a6b8c
SpasPandev <sppan@abv.bg>
2024-01-07 13:33:52 +0200

    added deleteMovie()

--- src/main/java/com/spaspandev/movieapp/controller/MovieController.java
+++ src/main/java/com/spaspandev/movieapp/controller/MovieController.java
@@ -3,9 +3,7 @@ package com.spaspandev.movieapp.controller;
 import com.spaspandev.movieapp.dto.DayTrendingMoviesDto;
 import com.spaspandev.movieapp.service.MovieService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/movie")
@@ -18,8 +16,14 @@ public class MovieController {
     }
 
     @GetMapping("/day-trending")
-    public ResponseEntity<DayTrendingMoviesDto> getDayTrendingMovies(){
+    public ResponseEntity<DayTrendingMoviesDto> getDayTrendingMovies() {
 
         return ResponseEntity.ok(movieService.getDayTrendingMovies());
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> deleteMovie(@PathVariable Long id) {
+
+        return movieService.deleteMovie(id);
     }
 }
--- src/main/java/com/spaspandev/movieapp/service/MovieService.java
+++ src/main/java/com/spaspandev/movieapp/service/MovieService.java
@@ -1,8 +1,11 @@
 package com.spaspandev.movieapp.service;
 
 import com.spaspandev.movieapp.dto.DayTrendingMoviesDto;
+import com.spaspandev.movieapp.repository.MovieRepository;
 import com.spaspandev.movieapp.utils.Url;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
@@ -10,16 +13,30 @@ import org.springframework.web.client.RestTemplate;
 public class MovieService {
 
     private final RestTemplate restTemplate;
+    private final MovieRepository movieRepository;
 
     @Value("${api_key}")
     private String api;
 
-    public MovieService(RestTemplate restTemplate) {
+    public MovieService(RestTemplate restTemplate, MovieRepository movieRepository) {
         this.restTemplate = restTemplate;
+        this.movieRepository = movieRepository;
     }
 
     public DayTrendingMoviesDto getDayTrendingMovies() {
 
         return restTemplate.getForObject(Url.ALL_DAY_TRENDING_MOVIES_URL + "?api_key=" + api, DayTrendingMoviesDto.class);
+    }
+
+    public ResponseEntity<?> deleteMovie(Long id) {
+
+        if (movieRepository.findById(id).isPresent()) {
+
+            movieRepository.deleteById(id);
+            return ResponseEntity.ok("Movie with id: " + id + " is deleted!");
+        } else {
+
+            return new ResponseEntity<>("Movie with id: " + id + " was not found!", HttpStatus.NOT_FOUND);
+        }
     }
 }
