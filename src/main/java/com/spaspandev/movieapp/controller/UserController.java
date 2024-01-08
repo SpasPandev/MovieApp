6acfeeb0905e435ec043d042954190ebdecbcc2a
SpasPandev <sppan@abv.bg>
2024-01-08 23:07:26 +0200

    added editProfile

--- src/main/java/com/spaspandev/movieapp/controller/UserController.java
+++ src/main/java/com/spaspandev/movieapp/controller/UserController.java
@@ -1,8 +1,12 @@
 package com.spaspandev.movieapp.controller;
 
+import com.spaspandev.movieapp.config.AppUser;
+import com.spaspandev.movieapp.dto.EditProfileDto;
 import com.spaspandev.movieapp.dto.UserDto;
 import com.spaspandev.movieapp.service.UserService;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -18,26 +22,32 @@ public class UserController {
     }
 
     @GetMapping("/admin/getAllUsers")
-    public ResponseEntity<List<UserDto>> getAllUsers(){
+    public ResponseEntity<List<UserDto>> getAllUsers() {
 
         return ResponseEntity.ok(userService.getAllUsers());
     }
 
     @PatchMapping("/admin/changeUserRole/{id}")
-    public ResponseEntity<UserDto> changeUserRole(@PathVariable Long id){
+    public ResponseEntity<UserDto> changeUserRole(@PathVariable Long id) {
 
         return ResponseEntity.ok(userService.changeUserRole(id));
     }
 
     @PatchMapping("/admin/deleteUser/{id}")
-    public ResponseEntity<?> deleteUser(@PathVariable Long id){
+    public ResponseEntity<?> deleteUser(@PathVariable Long id) {
 
         return userService.deleteUser(id);
     }
 
     @PatchMapping("/admin/restoreUser/{id}")
-    public ResponseEntity<?> restoreUser(@PathVariable Long id){
+    public ResponseEntity<?> restoreUser(@PathVariable Long id) {
 
         return userService.restoreUser(id);
+    }
+
+    @PatchMapping("/editProfile")
+    public ResponseEntity<?> editProfile(@Validated @RequestBody EditProfileDto editProfileDto, @AuthenticationPrincipal AppUser appUser) {
+
+        return userService.editProfile(editProfileDto, appUser);
     }
 }
--- src/main/java/com/spaspandev/movieapp/service/UserService.java
+++ src/main/java/com/spaspandev/movieapp/service/UserService.java
@@ -1,15 +1,18 @@
 package com.spaspandev.movieapp.service;
 
+import com.spaspandev.movieapp.config.AppUser;
+import com.spaspandev.movieapp.dto.EditProfileDto;
 import com.spaspandev.movieapp.dto.UserDto;
 import com.spaspandev.movieapp.enumeration.Role;
 import com.spaspandev.movieapp.model.entity.User;
 import com.spaspandev.movieapp.repository.UserRepository;
 import org.modelmapper.ModelMapper;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
@@ -18,10 +21,12 @@ public class UserService {
 
     private final UserRepository userRepository;
     private final ModelMapper modelMapper;
+    private final PasswordEncoder passwordEncoder;
 
-    public UserService(UserRepository userRepository, ModelMapper modelMapper) {
+    public UserService(UserRepository userRepository, ModelMapper modelMapper, PasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
         this.modelMapper = modelMapper;
+        this.passwordEncoder = passwordEncoder;
     }
 
     public List<UserDto> getAllUsers() {
@@ -51,7 +56,7 @@ public class UserService {
 
         Optional<User> userOpt = userRepository.findById(id);
 
-        if(userOpt.isEmpty() || userOpt.get().isDeleted()){
+        if (userOpt.isEmpty() || userOpt.get().isDeleted()) {
 
             return ResponseEntity.notFound().build();
         }
@@ -66,7 +71,7 @@ public class UserService {
 
         Optional<User> userOpt = userRepository.findById(id);
 
-        if(userOpt.isEmpty()){
+        if (userOpt.isEmpty()) {
 
             return ResponseEntity.notFound().build();
         }
@@ -75,5 +80,24 @@ public class UserService {
         userRepository.save(userOpt.get());
 
         return ResponseEntity.ok().build();
+    }
+
+    public ResponseEntity<?> editProfile(EditProfileDto editProfileDto, AppUser appUser) {
+
+        if (!Objects.equals(editProfileDto.getPassword(), editProfileDto.getConfirmPassword())) {
+
+            return ResponseEntity.badRequest().build();
+        }
+
+        User currentUser = userRepository.findByUsername(appUser.getUsername()).orElseThrow(() -> new RuntimeException("User with username: " +
+                appUser.getUsername() + "was not found!"));
+
+        currentUser.setUsername(editProfileDto.getUsername());
+        currentUser.setPassword(passwordEncoder.encode(editProfileDto.getPassword()));
+        currentUser.setEmail(editProfileDto.getEmail());
+
+        userRepository.save(currentUser);
+
+        return ResponseEntity.ok(modelMapper.map(currentUser, UserDto.class));
     }
 }
